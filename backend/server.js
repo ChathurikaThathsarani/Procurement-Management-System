276ffd07213921754208e85fbbd26f93fce7f3f2
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-10-10 18:21:53 +0530

    Add files to backend

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./config/db");
-const adminRoutes = require("./routes/adminRoutes");
-const trainerRoutes = require("./routes/trainerRoutes");
-const customerRoutes = require("./routes/customerRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -17,10 +14,6 @@ app.use("*", cors());
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
-
-app.use("/user/admin", adminRoutes);
-app.use("/user/trainer", trainerRoutes);
-app.use("/user/customer", customerRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
