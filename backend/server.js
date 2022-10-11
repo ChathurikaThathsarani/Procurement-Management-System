b253fec5fbf150cd0632564bf66e583ff0535ade
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-11 23:53:43 +0530

    updates server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./config/db");
+const siteManagerRoutes = require("./routes/siteManagerRoutes");
+const supplierRoutes = require("./routes/supplierRoutes");
+const staffRoutes = require("./routes/staffRoutes.js");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -12,11 +15,17 @@ app.use(express.json());
 app.use("*", cors());
 
 app.get("/", (req, res) => {
-  res.send("API is Running");
+	res.send("API is Running");
 });
+
+app.use("/user/manager", siteManagerRoutes);
+app.use("/user/supplier", supplierRoutes);
+app.use("/user/staff", staffRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Started on port ${PORT}..`));
