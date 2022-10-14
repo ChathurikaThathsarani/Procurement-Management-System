872e2a611fba9a748ba8b668f94ae3f54cdbfce2
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-10-14 23:17:13 +0530

    Add order routes

--- backend/routes/siteManagerRoutes.js
+++ backend/routes/siteManagerRoutes.js
@@ -6,6 +6,17 @@ const {
 	updateSiteManagerProfile,
 	deleteSiteManagerProfile,
 } = require("../controllers/siteManagerController");
+const {
+	createOrder,
+	getSuppliers,
+	getDraftOrders,
+	deleteDraftOrder,
+	getOneOrder,
+	getProductListOfSupplier,
+	draftOrderToPending,
+	getOrders,
+	pendingOrderToApproved,
+} = require("../controllers/orderController");
 const { protect } = require("../middleware/authSiteManagerMiddleware");
 const router = express.Router();
 
@@ -14,5 +25,15 @@ router.route("/login").post(authSiteManager);
 router.route("/view").get(protect, getSiteManagerProfile);
 router.route("/edit").put(protect, updateSiteManagerProfile);
 router.route("/delete").delete(protect, deleteSiteManagerProfile);
+
+router.route("/order/draft/create").post(protect, createOrder);
+router.route("/order/draft/suppliers").get(protect, getSuppliers);
+router.route("/orders/draft/:id").get(protect, getDraftOrders);
+router.route("/order/draft/delete/:id").delete(protect, deleteDraftOrder);
+router.route("/order/:id").get(protect, getOneOrder);
+router.route("/orders/draft/products/:id").get(protect, getProductListOfSupplier);
+router.route("/order/pending/:id").put(protect, draftOrderToPending);
+router.route("/orders/:id").get(protect, getOrders);
+router.route("/order/approved/:id").put(protect, pendingOrderToApproved);
 
 module.exports = router;
