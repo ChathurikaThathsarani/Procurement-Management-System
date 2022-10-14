dc6cc1b673d2ee6866b3d569ec27ed6735cae306
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-10-14 23:12:02 +0530

    Create a model for product

---
+++ backend/models/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+	{
+		supplier: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "Supplier",
+		},
+		productName: {
+			type: String,
+			required: true,
+		},
+		productPrice: {
+			type: Number,
+			required: true,
+		},
+		productDescription: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
