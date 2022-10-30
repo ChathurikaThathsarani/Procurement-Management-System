913ff82c5fb7138cd54336e009d091afca2233a1
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-30 20:43:34 +0530

    created home screen

---
+++ mobileapp/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
