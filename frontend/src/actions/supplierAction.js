10e9d66adc0ae76cff83c98456f1f1dff8a371ba
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-31 16:04:46 +0530

    updated supplier actions

--- frontend/src/actions/supplierAction.js
+++ frontend/src/actions/supplierAction.js
@@ -1,62 +1,122 @@
 import {
-  SUPPLIER_LOGIN_FAIL,
-  SUPPLIER_LOGIN_REQUEST,
-  SUPPLIER_LOGIN_SUCCESS,
-  SUPPLIER_LOGOUT,
+	SUPPLIER_LOGIN_FAIL,
+	SUPPLIER_LOGIN_REQUEST,
+	SUPPLIER_LOGIN_SUCCESS,
+	SUPPLIER_LOGOUT,
+	SUPPLIER_REGISTER_FAIL,
+	SUPPLIER_REGISTER_REQUEST,
+	SUPPLIER_REGISTER_SUCCESS,
 } from "../constants/supplierConstant";
 import axios from "axios";
 import swal from "sweetalert";
 
 export const supplierLogin = (nic, password) => async (dispatch) => {
-  try {
-    dispatch({ type: SUPPLIER_LOGIN_REQUEST });
+	try {
+		dispatch({ type: SUPPLIER_LOGIN_REQUEST });
 
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-      },
-    };
+		const config = {
+			headers: {
+				"Content-type": "application/json",
+			},
+		};
 
-    const { data } = await axios.post(
-      "/user/supplier/login",
-      { nic, password },
-      config
-    );
+		const { data } = await axios.post("/user/supplier/login", { nic, password }, config);
 
-    dispatch({ type: SUPPLIER_LOGIN_SUCCESS, payload: data });
-    swal({
-      title: "Success !!!",
-      text: "Supplier Log In Successful.",
-      icon: "success",
-      timer: 2000,
-      button: false,
-    });
-    setTimeout(function () {
-      window.location.href = "/supplier";
-    }, 2000);
-    localStorage.setItem("supplierInfo", JSON.stringify(data));
-  } catch (error) {
-    dispatch({
-      type: SUPPLIER_LOGIN_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
+		dispatch({ type: SUPPLIER_LOGIN_SUCCESS, payload: data });
+		swal({
+			title: "Success !!!",
+			text: "Supplier Log In Successful.",
+			icon: "success",
+			timer: 2000,
+			button: false,
+		});
+		setTimeout(function () {
+			window.location.href = "/supplier";
+		}, 2000);
+		localStorage.setItem("supplierInfo", JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: SUPPLIER_LOGIN_FAIL,
+			payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+		});
+	}
 };
 
 export function authHeader() {
-  let supplier = JSON.parse(localStorage.getItem("supplierInfo"));
+	let supplier = JSON.parse(localStorage.getItem("supplierInfo"));
 
-  if (supplier && supplier.token) {
-    return { Authorization: `Bearer ${supplier.token}` };
-  } else {
-    return {};
-  }
+	if (supplier && supplier.token) {
+		return { Authorization: `Bearer ${supplier.token}` };
+	} else {
+		return {};
+	}
 }
 
 export const supplierLogout = () => async (dispatch) => {
-  localStorage.removeItem("supplierInfo");
-  dispatch({ type: SUPPLIER_LOGOUT });
+	localStorage.removeItem("supplierInfo");
+	dispatch({ type: SUPPLIER_LOGOUT });
 };
+
+export const supplierRegister =
+	(
+		supplierId,
+		ownerName,
+		dob,
+		nic,
+		gender,
+		telephone,
+		companyName,
+		companyAddress,
+		email,
+		password,
+		pic,
+		suppliyingMaterials
+	) =>
+	async (dispatch) => {
+		try {
+			dispatch({ type: SUPPLIER_REGISTER_REQUEST });
+
+			const config = {
+				headers: {
+					"Content-type": "application/json",
+				},
+			};
+
+			const { data } = await axios.post(
+				`/user/supplier/register`,
+				{
+					supplierId,
+					ownerName,
+					dob,
+					nic,
+					gender,
+					telephone,
+					companyName,
+					companyAddress,
+					email,
+					password,
+					pic,
+					suppliyingMaterials,
+				},
+				config
+			);
+
+			dispatch({ type: SUPPLIER_REGISTER_SUCCESS, payload: data });
+			swal({
+				title: "Success !!!",
+				text: "Supplier Registration Successful.",
+				icon: "success",
+				timer: 2000,
+				button: false,
+			});
+
+			setTimeout(function () {
+				window.location.href = "/staff";
+			}, 2000);
+		} catch (error) {
+			dispatch({
+				type: SUPPLIER_REGISTER_FAIL,
+				payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+			});
+		}
+	};
