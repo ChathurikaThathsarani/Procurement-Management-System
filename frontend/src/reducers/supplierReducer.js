28b9ca73eb1a0111bfc433917247554a933e27bb
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-10-31 16:06:04 +0530

    updated supplier reducer

--- frontend/src/reducers/supplierReducer.js
+++ frontend/src/reducers/supplierReducer.js
@@ -1,22 +1,38 @@
 import {
-  SUPPLIER_LOGIN_FAIL,
-  SUPPLIER_LOGIN_REQUEST,
-  SUPPLIER_LOGIN_SUCCESS,
-  SUPPLIER_LOGOUT,
+	SUPPLIER_LOGIN_FAIL,
+	SUPPLIER_LOGIN_REQUEST,
+	SUPPLIER_LOGIN_SUCCESS,
+	SUPPLIER_LOGOUT,
+	SUPPLIER_REGISTER_FAIL,
+	SUPPLIER_REGISTER_REQUEST,
+	SUPPLIER_REGISTER_SUCCESS,
 } from "../constants/supplierConstant";
 
 export const supplierLoginReducer = (state = {}, action) => {
-  switch (action.type) {
-    case SUPPLIER_LOGIN_REQUEST:
-      return { loading: true };
-    case SUPPLIER_LOGIN_SUCCESS:
-      return { loading: false, supplierInfo: action.payload };
-    case SUPPLIER_LOGIN_FAIL:
-      return { loading: false, error: action.payload };
-    case SUPPLIER_LOGOUT:
-      return {};
+	switch (action.type) {
+		case SUPPLIER_LOGIN_REQUEST:
+			return { loading: true };
+		case SUPPLIER_LOGIN_SUCCESS:
+			return { loading: false, supplierInfo: action.payload };
+		case SUPPLIER_LOGIN_FAIL:
+			return { loading: false, error: action.payload };
+		case SUPPLIER_LOGOUT:
+			return {};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
+};
+
+export const supplierRegisterReducer = (state = {}, action) => {
+	switch (action.type) {
+		case SUPPLIER_REGISTER_REQUEST:
+			return { loading: true };
+		case SUPPLIER_REGISTER_SUCCESS:
+			return { loading: false, supplierInfo: action.payload };
+		case SUPPLIER_REGISTER_FAIL:
+			return { loading: false, error: action.payload };
+		default:
+			return state;
+	}
 };
