60e0ccbc19a3258a1353183b009aa322f5354be7
Charith Lakshan <it20606374@my.sliit.lk>
2022-10-31 23:45:00 +0530

    Added reducers to store file

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,8 @@ import {
   supplierOrderListReducer,
   orderToPlacedReducer,
   placedOrderListReducer,
+  OrderStaffListReducer,
+  StaffOrderToApproveReducer,
 } from "./reducers/orderReducer";
 
 import {
@@ -55,6 +57,9 @@ const reducer = combineReducers({
   Site_Management_Create: SiteCreateReducer,
   site_Management_Update: SiteUpdateReducer,
   site_Management_delete: SiteDeleteReducer,
+
+  order_List_Staff: OrderStaffListReducer,
+  Staff_order_To_Approve: StaffOrderToApproveReducer,
 });
 
 const siteManagerInfoFromStorage = localStorage.getItem("siteManagerInfo")
