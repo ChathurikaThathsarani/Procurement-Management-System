5f00dce552217de02f3ab74098ea41e4f7b4ce3c
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-11-02 23:02:01 +0530

    updated the mobile app version

---
+++ mobileapp/screens/SplashScreen.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet, Image } from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const SplashScreen = ({ navigation }) => {
+	const [animating, setAnimating] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setAnimating(false);
+			AsyncStorage.getItem("user_id").then((value) =>
+				navigation.replace(value === null ? "Auth" : "DrawerNavigationRoutes")
+			);
+		}, 5000);
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Image source={require("../Image/splash.png")} style={{ width: "90%", resizeMode: "contain", margin: 30 }} />
+			<ActivityIndicator animating={animating} color="#FFFFFF" size="large" style={styles.activityIndicator} />
+		</View>
+	);
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#307ecc",
+	},
+	activityIndicator: {
+		alignItems: "center",
+		height: 80,
+	},
+});
---
+++ mobileapp/services/orderservice.js
@@ -0,0 +1,139 @@
+import axios from "axios";
+
+import { autheader, api_base_url, getLoginUser } from "./utils";
+
+const getOrders = async () => {
+	try {
+		let userdata = await getLoginUser();
+		let header = await autheader();
+
+		const { data } = await axios.get(api_base_url + "orders/" + userdata._id, header);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Orders retrival failed");
+	}
+};
+
+const getDraftOrders = async () => {
+	try {
+		let userdata = await getLoginUser();
+		let header = await autheader();
+
+		const { data } = await axios.get(api_base_url + "orders/draft/" + userdata._id, header);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error.message);
+		throw new Error("Orders retrival failed");
+	}
+};
+
+const addDraftOrder = async (supplierName, placedDate, requiredDate) => {
+	try {
+		let header = await autheader();
+		let userdata = await getLoginUser();
+		let siteManagerId = userdata._id;
+		const { data } = await axios.post(
+			api_base_url + "order/draft/create",
+			{ siteManagerId, supplierName, placedDate, requiredDate },
+			header
+		);
+		console.log("here");
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Draft Order creation failed");
+	}
+};
+const deleteDraftOrder = async (id) => {
+	try {
+		let header = await autheader();
+
+		const { data } = await axios.delete(api_base_url + "order/draft/delete/" + id, header);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Order deletion failed");
+	}
+};
+const getOrder = async (id) => {
+	try {
+		let header = await autheader();
+
+		const { data } = await axios.get(api_base_url + "order/" + id, header);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Order retrival failed");
+	}
+};
+const getProductForOrder = async (id) => {
+	try {
+		let header = await autheader();
+
+		const { data } = await axios.get(api_base_url + "orders/draft/products/" + id, header);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Products retrival failed");
+	}
+};
+const getSuppliers = async () => {
+	try {
+		let header = await autheader();
+
+		const { data } = await axios.get(api_base_url + "order/draft/suppliers", header);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Supplier retrival failed");
+	}
+};
+
+const approveOrder = async (id, status) => {
+	try {
+		let header = await autheader();
+
+		const { data } = await axios.put(api_base_url + "order/approved/" + id, { status }, header);
+		console.log("her");
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Order approval failed");
+	}
+};
+const draftOrderToPending = async (orderid, productName, productQty) => {
+	try {
+		let header = await autheader();
+
+		const { data } = await axios.put(api_base_url + "order/pending/" + orderid, { productName, productQty }, header);
+		console.log("her");
+		console.log(data);
+		return data;
+	} catch (error) {
+		// console.log(error);
+		throw new Error("Operation failed");
+	}
+};
+
+const orderservice = {
+	getOrders,
+	getDraftOrders,
+	deleteDraftOrder,
+	getOrder,
+	approveOrder,
+	draftOrderToPending,
+	getProductForOrder,
+	addDraftOrder,
+	getSuppliers,
+};
+
+export default orderservice;
